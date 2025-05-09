853835e880b20ea0667060e5ba956c46100a18d9
ericness <ericness@users.noreply.github.com>
2025-05-09 00:56:39 +0000

    Cleaning up slides

--- generate_slides.sh
+++ generate_slides.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Convert the notebook to slides and output to the slides folder
-jupyter nbconvert end_of_json_validation_hell.ipynb --to slides --SlidesExporter.reveal_scroll=True
+jupyter nbconvert end_of_json_validation_hell.ipynb --to slides --SlidesExporter.reveal_scroll=True 
